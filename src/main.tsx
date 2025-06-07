356c8867996798d8ae0e687d4b761c42afdbd9d1
Brett Cassidy <brettcassidy@Bretts-MacBook-Pro.local>
2025-06-07 10:41:13 -0400

    main update

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react';
 // 1️⃣ Push the WebView below the iOS status bar:
 import { StatusBar } from '@capacitor/status-bar';
 
-
 // Immediately disable overlay so your UI starts below the notch
 (async () => {
   try {
@@ -11,8 +10,10 @@ import { StatusBar } from '@capacitor/status-bar';
     // ignore if running in a non-Capacitor/web context
   }
 })();
+
 import { createRoot } from 'react-dom/client';
 import { AppWrapper as App } from './App.tsx';
+import { SupabaseProvider } from './hooks/useSupabase'; // 🔥 ADD THIS IMPORT
 import { ThemeProvider } from './contexts/ThemeContext';
 import { TextSizeProvider } from './contexts/TextSizeContext';
 import { LanguageProvider } from './contexts/LanguageContext';
@@ -20,12 +21,14 @@ import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <LanguageProvider>
-      <ThemeProvider>
-        <TextSizeProvider>
-          <App />
-        </TextSizeProvider>
-      </ThemeProvider>
-    </LanguageProvider>
+    <SupabaseProvider> {/* 🔥 ADD THIS WRAPPER */}
+      <LanguageProvider>
+        <ThemeProvider>
+          <TextSizeProvider>
+            <App />
+          </TextSizeProvider>
+        </ThemeProvider>
+      </LanguageProvider>
+    </SupabaseProvider> {/* 🔥 CLOSE IT HERE */}
   </StrictMode>
 );
